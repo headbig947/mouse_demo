e21ef60b8ac69bfd4c163bf272365fe07f7d495f
chenzc <czc_947@163.com>
2025-12-10 17:33:11 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import router from './router'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)  // 关键：注册所有组件
+app.use(router)
 app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 配置路由映射
+const routes = [
+]
+
+// 创建路由实例
+const router = createRouter({
+  history: createWebHistory(), // 使用 HTML5 History 模式
+  routes
+})
+
+export default router
